34335e81796ab52dc00e1c238ee20cfefea342b5
Crissitx <camilocardenas2705@gmail.com>
2023-08-23 11:35:33 -0500

    Taller 2

--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Movie(models.Model):
@@ -6,3 +7,7 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to="movie/images")
     url = models.URLField(blank=True)
+class Premieres(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="premiere/images")
--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Movie
+from .models import Premieres
 
 # Create your views here.
 def home(request):
@@ -11,6 +12,9 @@ def home(request):
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    
+    premieres = Premieres.objects.all()
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies, 'premieres': premieres})
 def about(request):
     return render(request, 'about.html')
